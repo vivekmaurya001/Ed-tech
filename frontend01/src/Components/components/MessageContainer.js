3d631a2460942399970cbb1a3a246d229d976ca1
vivekmaurya001 <155618190+vivekmaurya001@users.noreply.github.com>
2024-04-11 01:26:51 +0530

    Update MessageContainer.js

--- frontend01/src/Components/components/MessageContainer.js
+++ frontend01/src/Components/components/MessageContainer.js
@@ -59,7 +59,7 @@ const MessageContainer = ({ fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.get(
-        `https://ed-tech-1-u5wg.onrender.com//api/message/${selectedChat._id}`,
+        `https://ed-tech-1-u5wg.onrender.com/api/message/${selectedChat._id}`,
         config1
       );
 
